200d5132481a20adf868a38ef555786b7fd2d37e
Ghyeok <gulww9997@naver.com>
2026-02-05 02:40:22 +0900

    refactor: 폴더, 파일 정리

---
+++ Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static bool _isShuttingDown = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_isShuttingDown)
+            {
+                Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' already destroyed. Returning null.");
+                return null;
+            }
+
+            if (_instance == null)
+            {
+                _instance = FindFirstObjectByType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject($"@{typeof(T).Name}");
+                    _instance = go.AddComponent<T>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnApplicationQuit()
+    {
+        _isShuttingDown = true;
+    }
+
+    protected virtual void OnDestroy()
+    {
+        _isShuttingDown = true;
+    }
+}
