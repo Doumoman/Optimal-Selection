1828a8ff3d05e145af93234abe429d8f3a6517ff
Ghyeok <gulww9997@naver.com>
2026-02-05 02:27:54 +0900

    feat: New Input System & InputManager

---
+++ Assets/Script/Core/InputManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : Singleton<InputManager>
+{
+    private GameControls _controls;
+
+    public event Action OnMenuPressed;
+
+    public Vector2 MoveDirection => _controls?.Player.Move.ReadValue<Vector2>() ?? Vector2.zero;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _controls = new GameControls();
+
+        // 이벤트 바인딩, 여기에 이벤트 추가
+        _controls.Player.Menu.performed += OnMenuPerformed;
+    }
+
+    private void OnEnable()
+    {
+        _controls?.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls?.Disable();
+    }
+
+    protected override void OnDestroy()
+    {
+        if (_controls != null)
+        {
+            _controls.Player.Menu.performed -= OnMenuPerformed;
+            _controls.Dispose();
+        }
+
+        base.OnDestroy(); // 부모의 종료 처리(플래그 설정 등) 실행
+    }
+
+    private void OnMenuPerformed(InputAction.CallbackContext context)
+    {
+        OnMenuPressed?.Invoke();
+    }
+}
