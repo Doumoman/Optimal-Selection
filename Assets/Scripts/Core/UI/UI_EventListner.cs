8b8dd1e4ce72299ef113b26ccebaad33f48a7b0e
Ghyeok <gulww9997@naver.com>
2026-02-07 18:30:10 +0900

    feat: UI_EventListner
    UI 입력과 관련된 이벤트들을 구독&해제하는 싱글톤

---
+++ Assets/Scripts/Core/UI/UI_EventListner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class UI_EventListener : Singleton<UI_EventListener>
+{
+    private void Start()
+    {
+        // UI 관련 입력 이벤트들 구독
+        Managers.Input.OnMenuPressed -= HandleEscKey;
+        Managers.Input.OnMenuPressed += HandleEscKey;
+    }
+
+    protected override void OnDestroy()
+    {
+        if (Managers.Instance != null && Managers.Input != null)
+        {
+            Managers.Input.OnMenuPressed -= HandleEscKey;
+        }
+    }
+
+    private void SetPlayerInput(bool allowInput)
+    {
+        // 현재 씬의 플레이어를 찾아서 설정
+        var player = FindFirstObjectByType<PlayerController>();
+        if (player != null)
+        {
+            player.isInputBlocked = !allowInput;
+        }
+    }
+
+    private void HandleEscKey()
+    {
+        int currentPopupCount = Managers.UI.PopupCount;
+        if (currentPopupCount > 0)
+        {
+            Managers.UI.ClosePopupUI();
+
+            if (Managers.UI.PopupCount == 0)
+            {
+                SetPlayerInput(true);
+            }
+        }
+        else
+        {
+            Managers.UI.ShowPopupUI<UI_Popup_Menu>("UI_Popup_Menu");
+            SetPlayerInput(false);
+        }
+    }
+}
