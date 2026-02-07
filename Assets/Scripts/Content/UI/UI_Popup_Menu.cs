1a76153c781bf87e637b6e234e793140020f1b35
Ghyeok <gulww9997@naver.com>
2026-02-07 18:31:01 +0900

    feat: UI_Popup_Menu
    Esc를 누르면 뜨는 메뉴창

---
+++ Assets/Scripts/Content/UI/UI_Popup_Menu.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UI_Popup_Menu : UI_Popup
+{
+    [SerializeField] private GameObject menuPanel;
+
+    [Header("Stats Info")]
+    [SerializeField] private TextMeshProUGUI txtLocation;
+    [SerializeField] private TextMeshProUGUI txtHP;
+    [SerializeField] private TextMeshProUGUI txtGold;
+
+    [Header("Button Info")]
+    [SerializeField] private GameObject[] menuButtons;
+
+    public override void Init()
+    {
+        base.Init();
+    }
+
+}
