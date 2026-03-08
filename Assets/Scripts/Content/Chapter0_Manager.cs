bbda87cee8b439b51d9c41e0a3691a2a98bb2143
Ghyeok <gulww9997@naver.com>
2026-03-08 16:32:03 +0900

    feat: ChapterManager

---
+++ Assets/Scripts/Content/Chapter0_Manager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Chapter0_Manager : ChapterManager
+{
+    [Header("Chapter 0 Specific")]
+    [SerializeField] private int _wrongInteractionCount = 0;
+
+    private readonly string stapiStartID = "Stapi_Tutorial_Start";
+
+    protected override void RegisterChapterBranches()
+    {
+        // 분기 추가
+        _branchRouters.Add(stapiStartID, CheckStapiInteraction);
+    }
+
+    public void AddWrongInteractionCount()
+    {
+        _wrongInteractionCount++;
+        Debug.Log($"[Chapter0] 딴짓 횟수 증가! 현재: {_wrongInteractionCount}회");
+    }
+
+    private string CheckStapiInteraction()
+    {
+        string interactionCountOver10 = "Stapi_Tutorial_Branch03_01";
+        string interactionCount7to9 = "Stapi_Tutorial_Branch02_01";
+        string interactionCountUnder7 = "Stapi_Tutorial_Branch01_01";
+
+        if (_wrongInteractionCount >= 10) return interactionCountOver10;
+        else if (_wrongInteractionCount >= 7) return interactionCount7to9;
+        else return interactionCountUnder7;
+    }
+}
--- Assets/Scripts/Content/Map/ChapterManager.cs
+++ Assets/Scripts/Content/Map/ChapterManager.cs
@@ -1,16 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ChapterManager : MonoBehaviour
+public abstract class ChapterManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Chapter Settings")]
+    public string chapterCSVFileName;
+
+    protected Dictionary<string, Func<string>> _branchRouters = new Dictionary<string, Func<string>>();
+
+    private void Start()
     {
-        
+        InitChapterDialogue();
+        RegisterChapterBranches();
+        Managers.Dialogue.OnBranchDecide += HandleChapterBranch;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected abstract void RegisterChapterBranches();
+
+    public void InitChapterDialogue()
     {
-        
+        if (string.IsNullOrEmpty(chapterCSVFileName))
+        {
+            Debug.Log("불러올 챕터 대화 CSV 파일 이름이 비어있습니다!");
+        }
+
+        Managers.Dialogue.LoadDialogueData(chapterCSVFileName);
     }
+
+    public string HandleChapterBranch(string currentID)
+    {
+        if (_branchRouters.TryGetValue(currentID, out Func<string> checkFunc))
+        {
+            return checkFunc.Invoke();
+        }
+        return null;
+    }
+
 }
