a962e3480ed3454007ff4d62bdc3bba3a794b5f8
Ghyeok <gulww9997@naver.com>
2026-03-17 18:21:35 +0900

    feat: ChapterSceneBinder

---
+++ Assets/Scripts/Content/Map/ChapterSceneBinder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ChapterSceneBinder : MonoBehaviour
+{
+    [SerializeField] private Transform chapterRoot;
+    [SerializeField] private Transform mapRoot;
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private Transform chapterUIRoot;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] ChapterManager chapterManager;
+    
+
+    private void Awake()
+    {
+        Debug.Log("[ChapterSceneBinder] RegisterScene 호출");
+        ChapterSceneRefs _chapterSceneRefs = new ChapterSceneRefs
+        {
+            chapterRoot = chapterRoot,
+            mapRoot = mapRoot,
+            playerController = playerController,
+            chapterUIRoot = chapterUIRoot,
+            mainCamera = mainCamera,
+            chapterManager = chapterManager
+        };
+
+        Managers.Map.RegisterScene(_chapterSceneRefs);
+        chapterManager.RegisterScene(_chapterSceneRefs);
+    }
+}
---
+++ Assets/Scripts/Content/Map/ChapterSceneRefs.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class ChapterSceneRefs
+{
+    public Transform chapterRoot;
+    public Transform mapRoot;
+    public PlayerController playerController;
+    public Transform chapterUIRoot;
+    public Camera mainCamera;
+    public ChapterManager chapterManager;
+}
